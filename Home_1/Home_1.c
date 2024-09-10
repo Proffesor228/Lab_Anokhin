cee4e9531017946e3984a09d2df8d83b8a36d187
Proffesor228 <antonisan228@mail.ru>
2024-09-11 02:53:55 +0300

    Add files via upload

---
+++ Home_1/Home_1.c
@@ -0,0 +1,12 @@
+#include <locale.h>
+#include <stdio.h>
+void main()
+{
+	setlocale(LC_CTYPE, "RUS");
+	puts("ֽאזלטעו Enter");
+	getchar();
+	puts("   ______   ");
+	puts(" _/__|___\\_ ");
+	puts("/____|_____\\");
+	puts(" 0        0  ");
+}
