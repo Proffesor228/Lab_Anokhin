c35ef0b17da42f7970ba64a26ba9455ce26ea7ef
Proffesor228 <antonisan228@mail.ru>
2024-09-24 01:06:34 +0300

    Add files via upload

---
+++ Lab3_Task3/Lab3_Task3.c
@@ -0,0 +1,17 @@
+﻿#include <locale.h>
+#include <stdio.h>
+
+int main()
+{
+	int a, b;
+	setlocale(LC_ALL, "RUS");
+	puts("Введите a и b");
+	scanf("%d %d", &a, &b);
+	printf("________________________\n");
+	printf("|a*b\t|a+b\t|a-b\t|\n");
+	printf("________________________\n");
+	printf("|%d*%d\t|%d+%d\t|%d-%d\t|\n", a, b, a, b, a, b);
+	printf("________________________\n");
+	printf("|%d\t|%d\t|%d\t|\n", a * b, a + b, a - b);
+	printf("________________________\n");
+}
